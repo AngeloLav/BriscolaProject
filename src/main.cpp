3abc7eacd2785fb6547599626e30f5fd8e23269a
AngeloLav <angelo.lavarini@studenti.unipd.it>
2026-08-19 23:39:36 +0200

    Now the program can read and show frames from a video

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,46 @@
+// Standard include
 #include <iostream>
+#include <string>
+
 #include <vector>
 
+// OpenCV
 #include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/core.hpp>
 
 
 int main(int argc, char** argv) {
-    std::cout << "Hello" << std::endl;
+
+    if (argc < 2) {
+        std::cout << "Use the program with: ./briscola <video.mp4>" << std::endl;
+        return 1;
+    }
+
+    // Analyzing video and check to see if the path is correct
+    std::string videoPath = argv[1];
+    cv::VideoCapture video(videoPath);
+    if (!video.isOpened()) {
+        std::cout << "Unable to open video: " << videoPath << std::endl;
+        return 1;
+    }
+
+    cv::Mat frame;
+    int frameIndex {0};
+
+    while (video.read(frame)){
+        std::cout << "Frame: " << frameIndex << " | Size:\nx = " << frame.cols << "\ny = " << frame.rows << std::endl;
+
+        cv::imshow("Briscola video", frame);
+
+        int key = cv::waitKey(30);
+
+        // If user presses ESC the video stops
+        if (key == 27) break;
+
+        frameIndex++;
+    }
+
     return 0;
 }
 
