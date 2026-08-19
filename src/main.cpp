1e4d1228f4630cca74a564068e676274bd234008
AngeloLav <angelo.lavarini@studenti.unipd.it>
2026-08-19 23:09:07 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+int main(int argc, char** argv) {
+    std::cout << "Hello" << std::endl;
+    return 0;
+}
+
+
