b78d0bb90b7018c0a8c5cfeed8bf2af52cb7a3e4
Luca ferraro <154615292+Luca2372@users.noreply.github.com>
2026-08-20 15:11:07 +0200

    base modello del gioco con gli enum da usare negli struct

---
+++ model/gameModels.h
@@ -0,0 +1,21 @@
+
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+
+enum class CardType {
+    DENARI,
+    BASTONI,
+    COPPE,
+    SPADE
+};
+
+// players identified by their position
+enum class Player {
+    NORTH,
+    SUD
+};
+
