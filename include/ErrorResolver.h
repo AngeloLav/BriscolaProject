6978530a462f5b38b385c28e655a6536befde189
Luca ferraro <154615292+Luca2372@users.noreply.github.com>
2026-08-25 23:33:53 +0200

    error resolver base

---
+++ include/ErrorResolver.h
@@ -0,0 +1,12 @@
+#include "gameModels.h"
+#include "Validator.h"
+
+
+class ErrorResolver {
+
+public:
+
+    // Tries to correct duplicated and missing cards using 3 methods:
+    static int resolveCardIssues(Game& game); // returns the number of corretions 
+
+};
---
+++ src/ErrorResolver.cpp
@@ -0,0 +1,10 @@
+#include "ErrorResolver.h"
+#include "GameEngine.h"
+
+int ErrorResolver::resolveCardIssues(Game& game) {
+
+    int corrections = 0;
+
+    return corrections;
+
+}
