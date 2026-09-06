7a331eb60fbaf5337246b59112605f4a155f07cd
Camilla Bellantuono <camilla.bellantuono@edu.unife.it>
2026-09-06 18:04:34 +0200

    Revert "Tolto le parti del calcolo del vincitore"
    This reverts commit a758775da94b59259666bc17ac2c9fb318386400.

--- src/analyzer.hpp
+++ src/analyzer.hpp
@@ -5,11 +5,10 @@
 #include <vector>
 #include <string>
 
-//the part of the code that is commented was used as test to see if it worked for calculating the winner of the round
-//int getCardPoints(int value);
-//int getCardRank(int value);
+int getCardPoints(int value);
+int getCardRank(int value);
 std::string suitToString(CardType type);
 Card getMostFreqCard(const std::vector<Card>& cards);
-//Player detWinner(Card northCard, Card southCard, Card briscolaCard, Player leader);
+Player detWinner(Card northCard, Card southCard, Card briscolaCard, Player leader);
 std::vector<CardDetected> getRankedCardsWithConfidence(const std::vector<Card>& cards);
 #endif
